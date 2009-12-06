44ce098b9f8da2b6b20d6b1011ff94c8aca393b9
Michael George <mdgeorge@cs.cornell.edu>
2009-12-06 18:04:46 -0500

    It builds and runs

--- src/gtans.cc
+++ src/gtans.cc
@@ -1,21 +1,16 @@
 #include <iostream>
-#include "algebra.h"
+#include <gtkmm.h>
 
 int main(int argc, char ** argv)
 {
-	std::cout << "SQRT1 = " << SQRT1 << std::endl;
-	std::cout << "SQRT2 = " << SQRT2 << std::endl;
-	std::cout << "SQRT3 = " << SQRT3 << std::endl;
-	std::cout << "SQRT6 = " << SQRT6 << std::endl;
-	std::cout << "COS45 = " << COS45 << std::endl;
-	std::cout << "SIN45 = " << SIN45 << std::endl;
-	std::cout << "COS30 = " << COS30 << std::endl;
-	std::cout << "SIN30 = " << SIN30 << std::endl;
-	std::cout << "ROT45 = " << ROT45 << std::endl;
-	std::cout << "ROT30 = " << ROT30 << std::endl;
-	std::cout << "ROT15 = " << ROT15 << std::endl;
-	std::cout << pow(ROT15, 24) << std::endl;
-	return 0;
+	Gtk::Main kit(argc, argv);
+
+	Glib::RefPtr<Gtk::Builder> builder = Gtk::Builder::create_from_file("../data/gtans.xml");
+
+	Gtk::Window * window = 0;
+	builder->get_widget("window", window);
+
+	kit.run(*window);
 }
 
 /*
