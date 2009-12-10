342da0cf5e5b0a782da9ab1202d5fd317a522778
Michael George <mdgeorge@cs.cornell.edu>
2009-12-09 23:02:54 -0500

    added test

---
+++ src/test.cc
@@ -0,0 +1,15 @@
+#include <complex>
+#include <iostream>
+
+#include "algebra.h"
+
+int main(int, char **)
+{
+	std::complex<Number> foo (0, 4);
+	// std::complex<float>  bar = foo;
+
+	// std::cout <<  bar << std::endl;
+
+	return 0;
+}
+
