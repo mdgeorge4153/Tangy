56644952475aaea704ad26b31b0fe14d3b678d4d
Michael George <mdgeorge@cs.cornell.edu>
2009-12-18 20:35:03 -0500

    Got CGAL test working

--- src/test.cc
+++ src/test.cc
@@ -1,15 +1,37 @@
-#include <complex>
+#include <CGAL/Nef_polyhedron_2.h>
+#include <CGAL/Extended_cartesian.h>
+#include <CGAL/Algebraic_structure_traits.h>
+#include <CGAL/Real_embeddable_traits.h>
+#include <CGAL/Quotient.h>
 #include <iostream>
-
 #include "algebra.h"
+
+namespace CGAL {
+	template<>
+	class Algebraic_structure_traits<Number>
+		: public Algebraic_structure_traits_base<Number, Field_tag>
+	{
+	};
+
+	template<>
+	class Real_embeddable_traits<Number>
+		: public INTERN_RET::Real_embeddable_traits_base<Number, Tag_true>
+	{
+	};
+}
+
+typedef Number FT;
+typedef CGAL::Extended_cartesian<FT>   Kernel;
+typedef CGAL::Nef_polyhedron_2<Kernel> Polygon;
+typedef Polygon::Point                 Point;
 
 int main(int, char **)
 {
-	std::complex<Number> foo (0, 4);
-	// std::complex<float>  bar = foo;
+	Point p1(0,0), p2(0,1), p3(1,0);
+	Point tri_points[3] = {p1, p2, p3};
 
-	// std::cout <<  bar << std::endl;
+	Polygon tri (tri_points, tri_points + 3);
 
-	return 0;
+	std::cout << tri;
 }
 
