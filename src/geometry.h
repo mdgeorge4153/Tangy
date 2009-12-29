bb9fb3fc9a215a356687c1fc5ffe898fa76ee83a
Michael George <mdgeorge@cs.cornell.edu>
2009-12-29 00:05:27 -0500

    Geometry skellington

--- src/geometry.h
+++ src/geometry.h
@@ -5,25 +5,30 @@
 #ifndef __tangy_geometry_h__
 #define __tangy_geometry_h__ 1
 
-namespace CGAL {
-	template<>
-	class Algebraic_structure_traits<ExtendedRational>
-		: public Algebraic_structure_traits_base<ExtendedRational, Field_tag>
-	{
-	};
+#include <CGAL/Nef_polyhedron_2.h>
 
-	template<>
-	class Real_embeddable_traits<ExtendedRational>
-		: public INTERN_RET::Real_embeddable_traits_base<ExtendedRational, Tag_true>
-	{
-	};
-}
 
-typedef ExtendedRational FT;
-typedef CGAL::Extended_cartesian<FT>   Kernel;
-typedef CGAL::Nef_polyhedron_2<Kernel> Polygon;
-typedef Polygon::Point                 Point;
+template<typename GameTraits>
+class ObstacleSet
+{
+public:
+	typedef typename GameTraits::point  point;
+	typedef typename GameTraits::tan    tan;
+	typedef typename GameTraits::tanset tanset;
 
+	ObstacleSet (const tan *, const tanset &);
+
+	point closest (const point &) const;
+
+private:
+	typedef typename GameTraits::number number;
+
+	typedef CGAL::Extended_cartesian<number> kernel;
+	typedef CGAL::Nef_polyhedron_2<kernel>   polygon;
+	typedef polygon::Point                   cgal_point;
+};
+
+#include "geometry.hcc"
 
 #endif
 
